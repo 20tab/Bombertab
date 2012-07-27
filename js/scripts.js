ce8f0e3fde7091f8b4e6a28d112f1bd933b0a1e4
Gabriele Giaccari <gabriele@20tab.com>
2012-07-27 19:56:20 +0300

    Update js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -214,9 +214,10 @@ jQuery(function(){
 			                      width: ACTOR_W, height: ACTOR_H})
 			                  .addSprite("bombBody_"+msg['p'],{animation: playerAnimation["bomb"],
 		                              posx: 0, posy: 0, width: ACTOR_W, height: ACTOR_H});
-		                    /*case "x":
+		                         break;
+		                    case "x":
 		            	    	$("#bomb_"+msg['p']).remove();
-		            	    	break;*/
+		            	    	break;
 				    default:
 				    	break;
 				}
