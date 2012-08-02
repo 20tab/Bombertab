028687d2f7bf8dd7eb0dc88ba0cd800efaedaea6
gabbork <gabriele@20tab.com>
2012-08-02 12:05:12 +0200

    aggiunti vari log

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,8 @@ jQuery(function(){
 	
 	function write_log(str, color){
 	    if(!color){color = 'black';}
-	    $("#log").prepend('<span style="color:'+color+'">'+str+'</span><br/>');
+    	var d = new Date();
+	    $("#log").prepend('<span style="color:'+color+'">'+d.getHours()+':'+d.getMinutes()+':'+d.getSeconds()+':'+d.getMilliseconds()+' '+str+'</span><br/>');
 	}
 	
 	/* /log */
@@ -184,9 +185,9 @@ jQuery(function(){
 				        $("#grid").html(arena(msg['a']));
 				        break;*/
 				    case "m": // m=move (muovi il player_id 'p' alle coordinate x y con direzione d)
-				        write_log('PLAYER: '+msg['p']+' | d: '+msg['d']+" - o: "+msg['o']);
+				        write_log('p: '+msg['p']+' | d: '+msg['d']+" - o: "+msg['o']);
 			            if(msg['d'] != msg['o']){   //se cambio direzione rispetto al frame precedente
-			                write_log('PLAYER: '+msg['p']+' | cambio dir','orange');
+			                write_log('p: '+msg['p']+' | cambio dir','orange');
                             switch(msg['d']){ //controllo la direzione nuova e imposto la nuova animation
 		                        case "n": //north
                                     $("#playerBody_"+msg['p']).setAnimation(playerAnimation["up"]);
@@ -216,7 +217,7 @@ jQuery(function(){
 		                         //$("#player_"+msg['p']).html($("#player_"+msg['p']).html()+msg['p']);
 		                         break;
                     case "k": // k=kill (rimuovi player_id 'p')
-                        write_log('PLAYER: '+msg['p']+' | muoio','red');                        
+                        write_log('p: '+msg['p']+' | muoio','red');                        
                         if(msg['p']==player_id){
                             CAN_MOVE = false;
                         }
@@ -225,15 +226,15 @@ jQuery(function(){
                             playerSound["die"].play();
                             $("#playerBody_"+msg['p']).setAnimation(playerAnimation["die"], 
         	    	            function(){
-        	    	                write_log('PLAYER: '+msg['p']+' | morendo','red');
+        	    	                write_log('p: '+msg['p']+' | morendo','red');
                                     $("#player_"+msg['p']).remove();
-                                    write_log('PLAYER: '+msg['p']+' | morto','red');
+                                    write_log('p: '+msg['p']+' | morto','red');
                                 }
                             );
                         }, 200);
             	        break;
             	    case "b": // b=bomb (disegna bombbody_id 'p' alle coordinate x y del player )
-            	        write_log('PLAYER: '+msg['p']+' | lascio la bomba '+'p: '+msg['p'],'red');
+            	        write_log('p: '+msg['p']+' | lascio la bomba '+'p: '+msg['p'],'red');
             	        if($("#bomb_"+msg['p']).get()){
             	            bombSound["drop"].play();
                 	    	$.playground().addGroup("bomb_"+msg['p'], {posx: msg['x'], posy: msg['y'],
@@ -246,20 +247,20 @@ jQuery(function(){
                          }        
                          break;
                     case "x": // x=explosion (esplode la bomba)
-                        write_log('PLAYER: '+msg['p']+' | esplode la bomba '+'p: '+msg['p'],'red');
+                        write_log('p: '+msg['p']+' | esplode la bomba '+'p: '+msg['p'],'red');
                         bombSound["loop"].pause();
                         bombSound["explode"].play();       
             	    	$("#bombBody_"+msg['p']).setAnimation(bombAnimation["explode"],
             	    	    function(){
-            	    	        write_log('PLAYER: '+msg['p']+' | rimuovendo la bomba '+'p: '+msg['p'],'red');
+            	    	        write_log('p: '+msg['p']+' | rimuovendo la bomba '+'p: '+msg['p'],'red');
             	    	        $("#bomb_"+msg['p']).remove();
-            	    	        write_log('PLAYER: '+msg['p']+' | rimossa la bomba '+'p: '+msg['p'],'red');                                 
+            	    	        write_log('p: '+msg['p']+' | rimossa la bomba '+'p: '+msg['p'],'red');                                 
             	    	   }
             	    	);
             	    	
             	    	break;
             	    case "0": // 0=stop (omino p fermo in x y con direzione 0)
-            	        write_log('PLAYER: '+msg['p']+' | idle','blue');
+            	        write_log('p: '+msg['p']+' | idle','blue');
             	        switch(msg['d']){  // controllo quale direzione viene passata
 				            case "n":
                                 $("#playerBody_"+msg['p']).setAnimation(playerAnimation["idle-n"]);
