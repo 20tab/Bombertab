fe62c455364012204317a0c6353bfe1ff0620f67
Leonida <roberto@20tab.com>
2013-06-20 17:13:17 +0200

    added J command

--- bombertab.py
+++ bombertab.py
@@ -308,6 +308,19 @@ class BomberTab(TremoloApp):
             # join the game
             print "new player", core_id
             return
+        elif msg['c'] == 'J':
+            bp = BomberPlayer(self, core_id, msg['a'])
+            lista_giocatori = []
+            for player in self.players.keys():
+                ep = self.players[player]
+                lista_giocatori.append([ep.id, ep.avatar, ep.x, ep.y])
+            # broadcast new player presence
+            announce = {'c':'p', 'p':bp.id, 'x':bp.x, 'y':bp.y}
+            self.broadcast(json.dumps(announce))
+
+            self.players[core_id] = bp
+            # join the game
+            print "player replay", core_id
         elif msg['c'] == 'n':
             try:
                 bp = self.players[msg['p']] 
